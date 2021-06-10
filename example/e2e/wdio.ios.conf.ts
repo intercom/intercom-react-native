1e832b2dc2b7ff0a77810d9f467508532bdea03d
pi0trpietruszewski <piotr.pietruszewski@intercom.consulting>
2021-06-10 20:03:44 +0200

    ci(ios): changed ios platform version

--- example/e2e/wdio.ios.conf.ts
+++ example/e2e/wdio.ios.conf.ts
@@ -12,7 +12,7 @@ exports.config = {
       browserName: '',
       automationName: 'XCUITest',
       platformName: 'iOS',
-      platformVersion: '14.4',
+      platformVersion: '14.3',
       deviceName: 'iPhone 8',
       app: 'build/Build/Products/Release-iphonesimulator/IntercomReactNativeExampleUI.app',
     },
