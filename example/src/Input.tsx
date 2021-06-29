4d0603a053dfe7b892584f867c4a4e147728ad37
piotr pietruszewski <piotr.pietruszewski@intercom.consulting>
2021-06-29 16:17:37 +0200

    refactor: changed example app layout

---
+++ example/src/Input.tsx
@@ -0,0 +1,29 @@
+import type { TextInputProps } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
+
+const Input = ({
+  title,
+  style,
+  ...props
+}: TextInputProps & { title: string }) => {
+  return (
+    <View style={styles.buttonContainer}>
+      {title && <Text style={styles.title}>{title}</Text>}
+      <TextInput {...props} style={[styles.input, style]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: { marginVertical: 8 },
+  title: { marginBottom: 3 },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+});
+
+export default Input;
