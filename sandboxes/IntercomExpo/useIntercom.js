24a103a07f96f450561570ba01d91d34fba187ea
Uddish Verma <uddish.verma@intercom.io>
2024-04-23 10:37:22 +0530

    Add a new expo app for react native (#188)
    * added expo app
    
    * Added README
    
    * removed app id
    
    * removed android and ios folders
    
    * pr review
    
    * added minimum deployment target for ios
    
    * install expo-system-ui

---
+++ sandboxes/IntercomExpo/useIntercom.js
@@ -0,0 +1,34 @@
+import Intercom from '@intercom/intercom-react-native';
+import { useMMKVStorage } from 'react-native-mmkv-storage';
+import { storage } from './App.utils';
+
+export function useIntercom() {
+  const [userIdentifier, setUserIdentifier] = useMMKVStorage('id', storage, '');
+
+  const [isLoggedIn, setIsLoggedIn] = useMMKVStorage('login', storage, false);
+
+  const handleLoginIdentifiedUser = async () => {
+    setIsLoggedIn(true);
+    await Intercom.loginUserWithUserAttributes({
+      email: userIdentifier,
+    });
+  };
+
+  const handleLoginUnidentifiedUser = async () => {
+    setIsLoggedIn(true);
+    await Intercom.loginUnidentifiedUser();
+  };
+
+  const handleLogout = async () => {
+    await Intercom.logout();
+    setIsLoggedIn(false);
+  };
+
+  return {
+    setUserIdentifier,
+    isLoggedIn,
+    handleLoginIdentifiedUser,
+    handleLoginUnidentifiedUser,
+    handleLogout,
+  };
+}
