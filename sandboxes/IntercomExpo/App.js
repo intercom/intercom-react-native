a55190c2f66abd8951746e2a00fbacf9a21b2021
Uddish Verma <uddish.verma@intercom.io>
2024-04-25 19:20:54 +0530

    Add intercom methods to test Expo sandbox app (#190)
    * added intercom fn
    
    * added more intercom methofs
    
    * remove keys

--- sandboxes/IntercomExpo/App.js
+++ sandboxes/IntercomExpo/App.js
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState, useCallback } from 'react';
 import {
-  StyleSheet,
   AppState,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  ScrollView,
 } from 'react-native';
 import Intercom, { Visibility } from '@intercom/intercom-react-native';
 import { useIntercom } from './useIntercom';
@@ -27,43 +26,200 @@ export default function App() {
     })();
   }, []);
 
+  const [conversationId, setConversationId] = useState(null);
+  const [articleId, setArticleId] = useState(null);
+  const [carouselId, setCarouselId] = useState(null);
+  const [surveyId, setSurveyId] = useState(null);
+  const [collectionId, setCollectionId] = useState(null);
+  const [launcherVisibility, setLauncherVisibility] = useState(false);
+
+  const showConversation = useCallback(() => {
+    openConversation(conversationId);
+  }, [conversationId]);
+
+  const showCarousel = useCallback(() => {
+    openCarousel(carouselId);
+  });
+
+  const showArticle = useCallback(() => {
+    openArticle(articleId);
+  }, [articleId]);
+
+  const showSurvey = useCallback(() => {
+    openSurvey(surveyId);
+  }, [articleId]);
+
+  const showHelpCenterCollection = useCallback(() => {
+    openHelpCenterCollection(collectionId);
+  }, [articleId]);
+
+  const toggleLauncherVisibility = useCallback(() => {
+    Intercom.setLauncherVisibility(
+      launcherVisibility ? Visibility.GONE : Visibility.VISIBLE
+    ).then(() => setLauncherVisibility((v) => !v));
+  });
+
   const {
     handleLoginIdentifiedUser,
     handleLoginUnidentifiedUser,
     handleLogout,
     setUserIdentifier,
-    isLoggedIn,
+    openHelpCenter,
+    openMessages,
+    openTicketsSpace,
+    openConversation,
+    openHelpCenterCollection,
+    openArticle,
+    openCarousel,
+    openSurvey,
+    openMessageComposer,
   } = useIntercom();
 
   return (
     <View style={styles.screenWrapper}>
-      <Text style={styles.title}>Push Notifications Sandbox</Text>
-      <View style={styles.wrapper}>
-        {!isLoggedIn ? (
+      <ScrollView>
+        <Text style={styles.title}>Push Notifications Sandbox</Text>
+        <View style={styles.wrapper}>
           <>
-            <TextInput style={styles.input} onChangeText={setUserIdentifier} />
+            <TextInput
+              style={styles.emailInput}
+              onChangeText={setUserIdentifier}
+              placeholder="Enter email"
+            />
             <TouchableOpacity
               style={styles.button}
               onPress={handleLoginIdentifiedUser}
             >
-              <Text style={styles.buttonText}>Login Identified User</Text>
+              <Text style={styles.buttonText}>REGISTER IDENTIFIED</Text>
             </TouchableOpacity>
+            <View style={styles.rowWrapper}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleLoginUnidentifiedUser}
+              >
+                <Text style={styles.buttonText}>REGISTER UNIDENTIFIED</Text>
+              </TouchableOpacity>
 
-            <View style={styles.divider} />
+              <TouchableOpacity style={styles.button} onPress={handleLogout}>
+                <Text style={styles.buttonText}>RESET</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <TouchableOpacity style={styles.button} onPress={openMessages}>
+                <Text style={styles.buttonText}>OPEN MESSAGES</Text>
+              </TouchableOpacity>
 
-            <TouchableOpacity
-              style={styles.button}
-              onPress={handleLoginUnidentifiedUser}
-            >
-              <Text style={styles.buttonText}>Login Unidentified User</Text>
-            </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={openHelpCenter}>
+                <Text style={styles.buttonText}>HELP CENTER</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={openTicketsSpace}
+              >
+                <Text style={styles.buttonText}>TICKETS</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={openTicketsSpace}
+              >
+                <Text style={styles.buttonText}>MESSENGER</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <TextInput
+                style={styles.input}
+                onChangeText={setCollectionId}
+                placeholder="Collection Id"
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={showHelpCenterCollection}
+                disabled={!collectionId}
+              >
+                <Text style={styles.buttonText}>HELP CENTER COLLECTION</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <TextInput
+                style={styles.input}
+                onChangeText={setConversationId}
+                placeholder="Conversation Id..."
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={showConversation}
+                disabled={!conversationId}
+              >
+                <Text style={styles.buttonText}>SHOW CONVERSATION</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.rowWrapper}>
+              <TextInput
+                style={styles.input}
+                onChangeText={setArticleId}
+                placeholder="Article Id..."
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={showArticle}
+                disabled={!articleId}
+              >
+                <Text style={styles.buttonText}>SHOW ARTICLE</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.rowWrapper}>
+              <TextInput
+                style={styles.input}
+                onChangeText={setCarouselId}
+                placeholder="Carousel Id..."
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={showCarousel}
+                disabled={!carouselId}
+              >
+                <Text style={styles.buttonText}>SHOW CAROUSEL</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.rowWrapper}>
+              <TextInput
+                style={styles.input}
+                onChangeText={setSurveyId}
+                placeholder="Survey Id..."
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={showSurvey}
+                disabled={!surveyId}
+              >
+                <Text style={styles.buttonText}>SHOW SURVEY</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.rowWrapper}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={openMessageComposer}
+              >
+                <Text style={styles.buttonText}>SHOW COMPOSER</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={toggleLauncherVisibility}
+              >
+                <Text style={styles.buttonText}>TOGGLE MESSENGER</Text>
+              </TouchableOpacity>
+            </View>
           </>
-        ) : (
-          <TouchableOpacity style={styles.button} onPress={handleLogout}>
-            <Text style={styles.buttonText}>Logout</Text>
-          </TouchableOpacity>
-        )}
-      </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
