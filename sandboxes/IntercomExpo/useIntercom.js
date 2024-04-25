aaf76abc85ad31e0babbf77bedf0148288107c5b
Uddish Verma <uddishverma22@gmail.com>
2024-04-25 14:54:15 +0530

    added intercom fn

--- sandboxes/IntercomExpo/useIntercom.js
+++ sandboxes/IntercomExpo/useIntercom.js
@@ -1,4 +1,7 @@
-import Intercom from '@intercom/intercom-react-native';
+import Intercom, {
+  Space,
+  IntercomContent,
+} from '@intercom/intercom-react-native';
 import { useMMKVStorage } from 'react-native-mmkv-storage';
 import { storage } from './App.utils';
 
@@ -24,11 +27,73 @@ export function useIntercom() {
     setIsLoggedIn(false);
   };
 
+  const openMessages = () => {
+    Intercom.presentSpace(Space.messages);
+  };
+
+  const openHelpCenter = () => {
+    Intercom.presentSpace(Space.helpCenter);
+  };
+
+  const openTicketsSpace = () => {
+    Intercom.presentSpace(Space.tickets);
+  };
+
+  const openMessenger = () => {
+    Intercom.present();
+  };
+
+  const openHelpCenterCollection = (collectionIds) => {
+    let helpCenterCollectionsContent =
+      IntercomContent.helpCenterCollectionsWithIds(collectionIds);
+    Intercom.presentContent(helpCenterCollectionsContent);
+  };
+
+  const openConversation = (conversationId) => {
+    let conversationContent =
+      IntercomContent.conversationWithConversationId(conversationId);
+    Intercom.presentContent(conversationContent);
+  };
+
+  const openArticle = (articleId) => {
+    let articleContent = IntercomContent.articleWithArticleId(articleId);
+    Intercom.presentContent(articleContent);
+  };
+
+  const openCarousel = (carouselId) => {
+    let carouselContent = IntercomContent.carouselWithCarouselId(carouselId);
+    Intercom.presentContent(carouselContent);
+  };
+
+  const openSurvey = (surveyId) => {
+    let surveyContent = IntercomContent.surveyWithSurveyId(surveyId);
+    Intercom.presentContent(surveyContent);
+  };
+
+  const toggleLauncher = (visibility) => {
+    Intercom.setLauncherVisibility(visibility);
+  };
+
+  const openMessageComposer = () => {
+    Intercom.presentMessageComposer('initial message');
+  };
+
   return {
     setUserIdentifier,
     isLoggedIn,
     handleLoginIdentifiedUser,
     handleLoginUnidentifiedUser,
     handleLogout,
+    openMessages,
+    openHelpCenter,
+    openTicketsSpace,
+    openMessenger,
+    openHelpCenterCollection,
+    openConversation,
+    openArticle,
+    openCarousel,
+    openSurvey,
+    toggleLauncher,
+    openMessageComposer,
   };
 }
